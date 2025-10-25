0acf67974854d98cbcc766a8d631c312a5ab70a4
Felix <felix.sch@protonmail.com>
2025-10-26 01:10:22 +0200

    removed console logs

--- src/app/loaders/ShaderLoader.mjs
+++ src/app/loaders/ShaderLoader.mjs
@@ -2,8 +2,5 @@
 export async function loadShaderSource(uri) {
     console.assert(typeof uri === 'string', 'loadShaderSource(): uri must be a string');
     const response = await fetch(`assets/shaders/${uri}`);
-    console.log(response.status, response.headers.get('content-type'));
-    const text = await response.text();
-    console.log(text);
-    return text;
+    return await response.text();
 }
